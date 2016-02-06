2b918539baa436be90080cd971adcacc9b7d606c
taysontey <taysontey@gmail.com>
2016-02-06 09:20:23 -0200

    Contracts(1)

---
+++ Projeto.Domain/Contracts/Repository/IRepositoryTime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto.Entity.Entities;
+
+namespace Projeto.Domain.Contracts.Repository
+{
+    public interface IRepositoryTime : IRepositoryBase<Time>
+    {
+    }
+}
